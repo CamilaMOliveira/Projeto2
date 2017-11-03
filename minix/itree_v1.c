a3c9479192e02dd902320be25d9ee0307e6be5ad
Camila Oliveira <camila.steffan@gmail.com>
2017-11-03 10:57:35 -0200

    printk nos arquivos

---
+++ minix/itree_v1.c
@@ -0,0 +1,80 @@
+#include <linux/buffer_head.h>
+#include <linux/slab.h>
+#include "minix.h"
+
+enum {DEPTH = 3, DIRECT = 7};	/* Only double indirect */
+
+typedef u16 block_t;	/* 16 bit, host order */
+
+static inline unsigned long block_to_cpu(block_t n)
+{
+	printk("@@@ block_to_cpu em itree_v1.c \n");
+
+	return n;
+}
+
+static inline block_t cpu_to_block(unsigned long n)
+{
+	printk("@@@ cpu_to_block em itree_v1.c \n");
+
+	return n;
+}
+
+static inline block_t *i_data(struct inode *inode)
+{
+	printk("@@@ i_data em itree_v1.c \n");
+
+	return (block_t *)minix_i(inode)->u.i1_data;
+}
+
+static int block_to_path(struct inode * inode, long block, int offsets[DEPTH])
+{
+	int n = 0;
+
+	printk("@@@ block_to_path em itree_v1.c \n");
+
+	if (block < 0) {
+		printk("MINIX-fs: block_to_path: block %ld < 0 on dev %pg\n",
+			block, inode->i_sb->s_bdev);
+	} else if (block >= (minix_sb(inode->i_sb)->s_max_size/BLOCK_SIZE)) {
+		if (printk_ratelimit())
+			printk("MINIX-fs: block_to_path: "
+			       "block %ld too big on dev %pg\n",
+				block, inode->i_sb->s_bdev);
+	} else if (block < 7) {
+		offsets[n++] = block;
+	} else if ((block -= 7) < 512) {
+		offsets[n++] = 7;
+		offsets[n++] = block;
+	} else {
+		block -= 512;
+		offsets[n++] = 8;
+		offsets[n++] = block>>9;
+		offsets[n++] = block & 511;
+	}
+	return n;
+}
+
+#include "itree_common.c"
+
+int V1_minix_get_block(struct inode * inode, long block,
+			struct buffer_head *bh_result, int create)
+{
+	printk("@@@ V1_minix_get_block em itree_v1.c \n");
+
+	return get_block(inode, block, bh_result, create);
+}
+
+void V1_minix_truncate(struct inode * inode)
+{
+	printk("@@@ V1_minix_truncate em itree_v1.c \n");
+
+	truncate(inode);
+}
+
+unsigned V1_minix_blocks(loff_t size, struct super_block *sb)
+{
+	printk("@@@ V1_minix_blocks em itree_v1.c \n");
+
+	return nblocks(size, sb);
+}
